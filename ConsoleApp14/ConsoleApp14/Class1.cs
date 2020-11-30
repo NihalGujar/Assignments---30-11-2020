9de4b2f74dd664bf31a3b8a37662de6a181d3522
NihalGujar <63520320+NihalGujar@users.noreply.github.com>
2020-11-30 23:48:35 +0530

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+     class Parent
+    {
+        protected virtual int ADD(int a,int b)
+        {
+            return a + b;
+        }
+        
+       
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Bank.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Bank
+    {
+        int rs;
+        int Netbalance;
+        public int NetBalance
+        {
+            get { return Netbalance; }
+            set { Netbalance = value; }
+        }
+
+        public Bank(int rs)
+        {
+            this.rs = rs;
+        }
+        public void deposit()
+        {
+            Netbalance = Netbalance + rs;
+            Console.WriteLine(Netbalance);
+            
+            //Console.WriteLine("task one");
+
+        }
+
+        public void withdrawal()
+        {
+            Netbalance = Netbalance - rs;
+            Console.WriteLine(Netbalance);
+            //Console.WriteLine("task two");
+        }
+    }
+}
